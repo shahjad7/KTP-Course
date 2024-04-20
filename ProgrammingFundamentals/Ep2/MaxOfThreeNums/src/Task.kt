d6631e8043ba5144b945b1d7b7e5d1d89c65ac6e
Shahjad Qureshi <shahjadqureshi.sq786@gmail.com>
2024-04-21 02:37:18 +0530

    MaxofThreeNumMiniApp

--- ProgrammingFundamentals/Ep2/MaxOfThreeNums/src/Task.kt
+++ ProgrammingFundamentals/Ep2/MaxOfThreeNums/src/Task.kt
@@ -1,7 +1,13 @@
 import com.faangx.ktp.basics.MaxOfThreeNumsMiniApp
 
 fun maxOf(x: Int, y: Int, z: Int): Int {
-    TODO()
+    if(x>y && x>z){
+        return x
+    }
+    else if (y>x && y>z)
+        return y
+    else
+        return z
 }
 
 fun main() {
