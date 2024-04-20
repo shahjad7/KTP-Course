2523287b76e28f676c603d4e2469dca65854e72f
Shahjad Qureshi <shahjadqureshi.sq786@gmail.com>
2024-04-21 03:16:44 +0530

    GradeCalculateApp

--- ProgrammingFundamentals/Ep2/GradeCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep2/GradeCalculator/src/Task.kt
@@ -1,7 +1,20 @@
 import com.faangx.ktp.basics.GradeCalculatorMiniApp
 
+
 fun gradeFor(m1: Int, m2: Int, m3: Int, m4: Int, m5: Int): String {
-    TODO()
+    val average = (m1+m2+m3+m4+m5)/50f
+
+    return when {
+      average in 9f..10f -> "A+"
+      average  in 8f..9f ->  "A"
+      average  in 7f..8f ->  "B"
+      average  in 6f..7f ->  "C"
+      average  in 5f..6f ->  "D"
+      average  in 3f..5f -> "E"
+
+        else -> "Fail"
+    }
+
 }
 
 fun main() {
