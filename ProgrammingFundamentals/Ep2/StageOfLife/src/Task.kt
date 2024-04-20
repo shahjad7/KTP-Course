8160a520f66d7914f63fadceda38c2134d049ba0
Shahjad Qureshi <shahjadqureshi.sq786@gmail.com>
2024-04-21 02:44:58 +0530

    StageOfLifeMiniApp

--- ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
+++ ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
@@ -1,7 +1,18 @@
 import com.faangx.ktp.basics.StageOfLifeMiniApp
 
 fun stageOfLife(age: Int): String {
-    TODO()
+    when (age){
+        in(0..2) ->  return "Infancy"
+        in(3..11) -> return "Childhood"
+        in(12..17) -> return "Teenage"
+        in(18..59) -> return "Adulthood"
+
+        else -> {
+            (age >=60)
+               return "Old age"
+
+        }
+    }
 }
 
 fun main() {
