51469ddf5d70d688076627359f69aabbb7a9b9dd
Shahjad Qureshi <shahjadqureshi.sq786@gmail.com>
2024-04-22 23:47:43 +0530

    FactorCalculationMiniApp

--- ProgrammingFundamentals/Ep3/FactorCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep3/FactorCalculator/src/Task.kt
@@ -1,11 +1,21 @@
 import com.faangx.ktp.basics.FactorCalculatorMiniApp
 
 fun printFactorsOf(num: Int) {
-    TODO()
+    for (i in 1..num){
+        if (num % i == 0){
+            print("$i, ")
+        }
+    }
 }
 
 fun isPrime(num: Int): Boolean {
-    TODO()
+    var noFactor = 0
+    for (i in 1..num){
+        if (num % i == 0){
+           ++noFactor
+        }
+    }
+    return noFactor == 2
 }
 
 fun main() {
--- ProgrammingFundamentals/Ep3/MultiplicationTableV1/src/Task.kt
+++ ProgrammingFundamentals/Ep3/MultiplicationTableV1/src/Task.kt
@@ -1,7 +1,9 @@
 import com.faangx.ktp.basics.MultiplicationTableMiniAppV1
 
 fun printTable(num: Int, start: Int, end: Int) {
-    TODO()
+    for (i in start..end){
+        println("$num X $i = ${num * i}")
+    }
 }
 
 fun main() {
