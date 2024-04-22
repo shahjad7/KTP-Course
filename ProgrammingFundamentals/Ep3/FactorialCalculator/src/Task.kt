864cb6072f4e4444bdce24beac833eeb1030c618
Shahjad Qureshi <shahjadqureshi.sq786@gmail.com>
2024-04-23 00:28:24 +0530

    factorCalculate App

--- ProgrammingFundamentals/Ep3/FactorialCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep3/FactorialCalculator/src/Task.kt
@@ -1,15 +1,24 @@
+import androidx.compose.ui.window.WindowPosition.PlatformDefault.x
 import com.faangx.ktp.basics.FactorialCalculatorMiniApp
 
 fun factorialOf(x: Long): Long {
-    TODO()
+    var factorials = 1L
+    for(i in 2..x){
+        factorials *= i
+    }
+    return factorials
 }
 
 fun permutationsOf(n: Long, r: Long): Long {
-    TODO()
+    var permutationss = 1L
+    for(i in (n-r+1.. n)){
+        permutationss *= i
+    }
+    return permutationss
 }
 
 fun combinationsOf(n: Long, r: Long): Long {
-    TODO()
+     return permutationsOf(n,r)/factorialOf(r)
 }
 
 fun main() {
