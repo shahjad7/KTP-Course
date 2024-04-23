f5a19b7bb65e390f4dc07232c1e995b4ffd2fff7
Shahjad Qureshi <shahjadqureshi.sq786@gmail.com>
2024-04-23 22:35:34 +0530

    TLNPatternMiniApp

--- ProgrammingFundamentals/Ep3/Pattern_TLN/src/Task.kt
+++ ProgrammingFundamentals/Ep3/Pattern_TLN/src/Task.kt
@@ -1,7 +1,12 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int) {
-    TODO()
+    repeat(lines){i ->
+        repeat(lines - i){
+            print(lines - i)
+        }
+        println()
+    }
 }
 
 fun main() {
