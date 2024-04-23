35d1cfe577e88801b33b3ee564308160b5c92664
Shahjad Qureshi <shahjadqureshi.sq786@gmail.com>
2024-04-23 23:44:34 +0530

    PD3PatterMiniApp

--- ProgrammingFundamentals/Ep3/Pattern_PD3/src/Task.kt
+++ ProgrammingFundamentals/Ep3/Pattern_PD3/src/Task.kt
@@ -1,7 +1,19 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
-    TODO()
+    repeat(lines){ i->
+        repeat(i) {print(' ')}
+
+        repeat(2 * (lines-i) - 1){j ->
+            if(j%2==0){
+                print(char1)
+            }
+            else{
+                print(char2)
+            }
+        }
+        println()
+    }
 }
 
 fun main() {
